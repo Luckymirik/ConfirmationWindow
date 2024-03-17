74eb1149e2f4068d2ad9dc1cac58adff4ed9da56
Miroslav Tsadkin <Luckymirik@mail.ru>
2024-03-17 14:35:18 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class Main {
     public static void main(String[] args) {
+        String q1 = JOptionPane.showInputDialog("Вы пойдете на выборы?(да/нет)");
+        if (q1.equals("да")) {
+            JOptionPane.showMessageDialog(null, "Крвсава!!!");
+        }else if (q1.equals("нет")){
+            JOptionPane.showMessageDialog(null,"Ну и зря!!!!");
+        }
+        String q2 = JOptionPane.showInputDialog("За Путина будешь голосовать?(да/нет)");
+        if (q2.equals("да")){
+            JOptionPane.showMessageDialog(null,"Ты шо гонишь!!!!");
+        }else if (q2.equals("нет")){
+            JOptionPane.showMessageDialog(null,"Шаришь!!!!");
+        }
+
+
+
+
+
+
 
     }
 }
